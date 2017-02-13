866e0b4d94ddb6162d6230029e58c2e1fe332bb0
buttercup11 <darksoulinheaven666@gmail.com>
2017-02-13 13:33:18 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -6,9 +6,10 @@ from preprocess import perform_preprocessing
 def ocr_operation(img_url):
 
     raw_image = cv2.imread(img_url, 0)
+    
+    # pre process the raw image for noise removal and
+    # smoothing and binarization
 
-    # get all the words (as an numpy image array), words on each line, and
-    # maximum height on that line
     image_for_det, image_for_ext = perform_preprocessing(raw_image)
 
     # to write the output into a file
